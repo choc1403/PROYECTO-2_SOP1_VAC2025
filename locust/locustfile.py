cc39efd28005909750b9d057f040d115f143f724
choc1403 <eloicx@gmail.com>
2025-12-23 00:08:46 -0600

    PROYECTO 2
    Locust del proyecto

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,26 +1,22 @@
-# Locustfile.py
 from locust import HttpUser, TaskSet, task, between
 import random
-import json
 
 class MyTasks(TaskSet):
-    
+
     @task(1)
-    def engineering(self):
+    def enviar_venta(self):
         categorias = ["Electronica", "Ropa", "Hogar", "Belleza"]
-    
-        # Datos de venta
+
         venta_data = {
-            "categoria": random.choices(categorias),  # Random Categoria
-            "producto_id": random.randint(1,10),  # Random de producto_id de 1 y 10
-            "precio": random.randint(40, 1000),  # Precio random entre 40 y 80
-            "cantidad_vendida": random.randint(1,20)  # Cantidad random entre 1 y 20
+            "categoria": random.choice(categorias),        # Seleccion random de categorias
+            "producto_id": f"P{random.randint(1,10)}",     # Random de productos entre 1 y 10
+            "precio": random.uniform(40, 1000),            # Random de precio permitiendo decimales entre 40 y 1000
+            "cantidad_vendida": random.randint(1, 20)      # Random de cantidades entre 1 y 20
         }
-        
-        # Envio de JSON hacia route como POST
-        headers = {'Content-Type': 'application/json'}
+
+        headers = {"Content-Type": "application/json"}
         self.client.post("/venta", json=venta_data, headers=headers)
 
 class WebsiteUser(HttpUser):
     tasks = [MyTasks]
-    wait_time = between(1, 5)  # Tiempo de espera entre tareas entre 1 y 5 segundos
+    wait_time = between(1, 5)
