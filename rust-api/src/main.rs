50bc2f95fd22f9a27f957cbb5f66c78038989e5a
choc1403 <eloicx@gmail.com>
2025-12-24 17:32:02 -0600

    PROYECTO 2
    k8s

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{post, web, App, HttpServer, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
+use std::io::Write;
 
 #[derive(Deserialize, Serialize)]
 struct Venta {
@@ -11,8 +12,8 @@ struct Venta {
 
 async fn enviar_a_go(venta: &Venta) -> Result<(), reqwest::Error> {
     let go_service_url = std::env::var("GO_SERVICE_URL")
-        .unwrap_or_else(|_| "http://localhost:8081".to_string());
-
+        .unwrap_or_else(|_| "http://go-api:8081".to_string());
+    
     let client = reqwest::Client::new();
 
     client
@@ -26,7 +27,6 @@ async fn enviar_a_go(venta: &Venta) -> Result<(), reqwest::Error> {
 
 #[post("/venta")]
 async fn recibir_venta(venta: web::Json<Venta>) -> impl Responder {
-
     if let Err(e) = enviar_a_go(&venta).await {
         eprintln!("Error enviando a Go: {}", e);
         return HttpResponse::InternalServerError().body("Error enviando a Go");
@@ -35,16 +35,16 @@ async fn recibir_venta(venta: web::Json<Venta>) -> impl Responder {
     HttpResponse::Ok().json("Venta procesada")
 }
 
-
-#[actix_web::main]
+#[actix_web::main] // Solo una vez
 async fn main() -> std::io::Result<()> {
-    println!("Inciando Servidor");
+    println!("Iniciando Servidor en puerto 8080...");
+    std::io::stdout().flush().unwrap(); 
+
     HttpServer::new(|| {
         App::new()
-            .service(recibir_venta) 
+            .service(recibir_venta)
     })
-    .bind(("0.0.0.0", 8080))?
-    .workers(4)
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
